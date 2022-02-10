6b733fab7e06e0d9b5824272f2f1ab0ec482a60d
Ju4nPx <juanpmachadob@gmail.com>
2022-02-09 22:58:34 -0500

    Added navbar component

---
+++ client-vue/src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="navbar">
+    <div class="container navbar-content">
+      <div class="navbar-head">
+        <a href="#" class="logo">My Game List</a>
+        <div class="mobile">
+          <div class="search">
+            <input class="search-btn" type="search" placeholder="Search..." />
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </div>
+          <div class="mobile-menu">
+            <a v-on:click="menuOpen = !menuOpen">
+              <i class="fas fa-bars"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+      <nav
+        class="navigation"
+        v-bind:class="{ 'mobile-menu-active': !menuOpen }"
+      >
+        <a href="" class="nav-item">Sign In</a>
+        <a href="" class="nav-item">Sign Up</a>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data: () => ({
+    menuOpen: false,
+  }),
+};
+</script>
