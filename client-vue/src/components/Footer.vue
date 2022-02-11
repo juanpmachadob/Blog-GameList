89474d92c0be84429900178503c43112541830ee
Ju4nPx <juanpmachadob@gmail.com>
2022-02-10 23:22:16 -0500

    Initial styles for popular games section finished

--- client-vue/src/components/Footer.vue
+++ client-vue/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    Footer
+    
   </footer>
 </template>
 
--- client-vue/src/views/Home.vue
+++ client-vue/src/views/Home.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>Home</div>
+  <main id="home" class="home">
+    <div class="container">
+      <div class="popular-desc">
+        <h1>Popular games</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+          temporibus.
+        </p>
+      </div>
+      <div class="popular-games flex-row">
+        <div v-for="index in 4" :key="index" class="game-item">
+          <figure class="game-image">
+            <img
+              src="@/assets/images/jumping-planet.jpg"
+              alt="Jumping Planet"
+            />
+            <figcaption>250 <i class="fas fa-thumbs-up"> </i></figcaption>
+          </figure>
+          <div class="game-description">
+            <h3>Jumping Planet</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur...</p>
+            <div>
+              <a href="" class="btn btn-primary">Ver</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="pagination">
+        <a href="#">❮</a>
+        <a href="#">❯</a>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
